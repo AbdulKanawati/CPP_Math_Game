dda68907124442bb9f6a1519d3b2439cb43e89f9
Abdul Kanawati <abode.knwaty@gmail.com>
2025-04-22 11:09:11 +0200

    feat(core): define enums for question level and operation type, and structs for Question and Quizz

--- CPP_Math_Game.cpp
+++ CPP_Math_Game.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
+#include<string>
+#include<cstdlib>
 
+using namespace std;
+
+// Enum for difficulty levels
+enum enQuestionLevel { EasyLevel = 1, MedLevel = 2, HardLevel = 3, Mix = 4 };
+
+// Enum for operation types
+enum enOperationType { Add = 1, Sub = 2, Mult = 3, Div = 4, MixOp = 5 };
+
+// Structure to hold information about a single question
+struct stQuestion
+{
+    int Number1 = 0;
+    int Number2 = 0;
+    enOperationType OperationType;
+    enQuestionLevel QuestionLevel;
+    int CorrectAnswer = 0;
+    int PlayerAnswer = 0;
+    bool AnswerResult = false;
+};
+
+// Structure to hold the whole quiz session
+struct stQuizz
+{
+    stQuestion QuestionList[100];
+    short NumberOfQuestions;
+    enQuestionLevel QuestionLevel;
+    enOperationType OpType;
+    short NumberOfWrongAnswers = 0;
+    short NumberOfRightAnswers = 0;
+    bool isPass = false;
+};
 
 int main() {
 
